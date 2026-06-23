aead4829504c8d4883ba3d30fea4a3b2967c492d
Karim <karintexxx@inbox.ru>
2026-06-23 17:24:32 +0300

    fix bugs

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function createWindow() {
         minWidth: 800,
         minHeight: 600,
         webPreferences: {
-            preload: path.join(__dirname, '../app.asar.unpacked/preload.js'),
+            preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: false,
             contextIsolation: true,
             sandbox: false,
